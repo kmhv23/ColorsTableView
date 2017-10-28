b0a72c2f042c94f94643dcea286fe00c0c8c91e8
kmhv23 <31312524+kmhv23@users.noreply.github.com>
2017-10-27 23:08:25 -0500

    Add files via upload

---
+++ ColorsTableView/ColorsTableView/ColorsTableView/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  ColorsTableView
+//
+//  Created by Kevin Hunkins on 10/27/17.
+//  Copyright © 2017 Kevin Hunkins. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+    
+    
+}
